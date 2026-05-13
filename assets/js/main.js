b08717bbadf82a464d0ea963923b33e24c09b1b0
Jessica Wang <jiaqicawang@gmail.com>
2026-05-13 15:33:26 -0400

    style!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/* 
-   Flirty Text Effect 
-   Cleaned up for Jessica's Portfolio 
-*/
-
 var speed = 100; // speed of effect, lower is faster
 var flitxt;
 var flipos = 0;
@@ -13,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var flirty = document.getElementById("flirt");
     
     if (flirty) {
-        // Get the text from your H1, add spaces to the ends
         flitxt = flirty.firstChild.nodeValue;
         flitxt = String.fromCharCode(160) + flitxt + String.fromCharCode(160);
         
@@ -22,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
             flirty.removeChild(flirty.childNodes[0]);
         }
         
-        // Break text into individual spans so they can move
         for (var i = 0; i < flitxt.length; i++) {
             var fliri = document.createElement("span");
             fliri.setAttribute("id", "flir" + i);
@@ -30,7 +23,6 @@ document.addEventListener("DOMContentLoaded", function() {
             flirty.appendChild(fliri);
         }
         
-        // Start the animation
         flitim = setInterval(flaunt, speed);
     }
 });
